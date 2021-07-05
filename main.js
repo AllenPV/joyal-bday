31b54f1d13c72d559a9d294c6ebe0fb931d92620
AllenPV <73430174+AllenPV@users.noreply.github.com>
2021-07-05 13:13:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+sound = "";
+function preload(){
+sound = loadSound("Best-Happy-Birthday-To-You-Happy-Birthday-Songs-Remix-2021-WishesPlus.com(1)_.mp3"); 
+}
+function click(){
+    sound.play();
+}
